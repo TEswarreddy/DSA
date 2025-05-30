6d25fda1682143bfffc7f394e3b1631cc6910b65
teswarreddy <thathieswarreddy@gmail.com>
2025-05-30 09:41:45 +0530

    Update initial array in SortColors implementation for testing

--- SortColors.java
+++ SortColors.java
@@ -50,7 +50,7 @@
 
 public class SortColors {
     public static void main(String[] args) {
-        int arr[] = {2, 0, 1, 2, 1};
+        int arr[] = {1,2};
         int low = 0;
         int mid = 0;
         int high = arr.length - 1;
